e8adeb60eab2802592b3adbc4fee61ae2fc639f0
Okinea Dev <hi@okinea.dev>
2025-03-15 07:38:08 +0100

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { Plugin, ViteDevServer } from 'vite'
 
-import fs from 'node:fs'
-import path from 'node:path'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 
 import pc from 'picocolors'
 import log from './logger'
