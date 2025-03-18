b24eda5feafb99187adedec9543203e42e30de47
Okinea Dev <hi@okinea.dev>
2025-03-18 07:57:28 +0100

    chore: :label: improve types

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,28 +2,45 @@ import type { UserConfig } from 'vite'
 
 export interface LlmstxtSettings {
 	/**
-	 * Determines whether to generate the `llms-full.txt` which contains all the documentation in one file.
+	 * Determines whether to generate the `llms-full.txt` which contains all the documentation in one file
 	 *
 	 * @default true
 	 */
 	generateLLMsFullTxt?: boolean
 	/**
-	 * Determines whether to generate the `llms.txt` which contains a list of sections with links.
+	 * Determines whether to generate the `llms.txt` which contains a list of sections with links
 	 *
 	 * @default true
 	 */
 	generateLLMsTxt?: boolean
 }
 
+/**
+ * Represents a prepared file with its title and path.
+ */
 export type PreparedFile = {
-	// content: string
+	/**
+	 * The title of the file
+	 *
+	 * @example 'Guide'
+	 */
 	title: string
-	// fileName: string
+
+	/**
+	 * The absolute path to the file
+	 *
+	 * @example 'guide/getting-started.md'
+	 */
 	path: string
 }
 
+/**
+ * Configuration interface for VitePress, extending the UserConfig from Vite.
+ */
 interface VitePressConfig extends UserConfig {
+	/** The **VitePress** configuration */
 	vitepress: {
+		/** The directory where the output files will be generated */
 		outDir: string
 	}
 }
