25caed5d6fb9bc9d123b713a48c21799ca919086
Okinea Dev <hi@okinea.dev>
2025-07-19 09:11:34 +0200

    ci: fix failing cspell checks

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,6 @@
 import type { Configuration } from 'lint-staged'
 
 export default {
-	'*': ['bun run format --no-errors-on-unmatched', 'cspell'],
+	'*': ['bun run format --no-errors-on-unmatched', 'cspell --no-error-on-empty'],
 	'src/**/*.ts': () => 'bun run lint:tsc',
 } as Configuration
