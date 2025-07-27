5e2b750fae00d75c50128545fd6184b1859f4c7f
Okinea Dev <hi@okinea.dev>
2025-07-27 13:37:55 +0200

    refactor(build): simplify `bunup` configuration

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'bunup'
-// @ts-expect-error
 import { copy } from 'bunup/plugins'
 
 export default defineConfig({
 	entry: ['src/index.ts', 'src/vitepress-components/utils.ts'],
-	format: ['esm'],
 	// Generate declaration file (`.d.ts`)
 	dts: {
 		entry: ['src/index.ts'],
 		splitting: true,
 	},
-	minify: false,
-	banner: '// Built with bunup',
-	clean: true,
+	banner: '// Built with bunup (https://bunup.dev)',
 	plugins: [copy(['src/vitepress-components/*.vue'], 'dist/vitepress-components')],
 })
